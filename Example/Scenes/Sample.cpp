29d2128286aca1547a2664016ab0834b811e4025
LEEVGS <lee.vgs@hotmail.com>
2023-11-12 16:47:04 +0100

    Default scene now has a camera and directional light

--- Example/Scenes/Sample.cpp
+++ Example/Scenes/Sample.cpp
@@ -1,6 +1,16 @@
 #include "Sample.h"
 
+#include <SceneGraph/Scene.h>
+#include <Components/RenderComponents/CameraComponent.h>
+#include <Components/RenderComponents/DirectionalLightComponent.h>
+#include <Components/Transform/Transform.h>
+
 void Example::SampleScene::Load(leap::Scene& scene)
 {
-	scene;
+	const auto pCamera{ scene.CreateGameObject("Main camera") };
+	pCamera->AddComponent<leap::CameraComponent>()->SetAsActiveCamera(true);
+	pCamera->GetTransform()->SetWorldPosition(0, 0, -5);
+
+	const auto pDirLight{ scene.CreateGameObject("Directional light") };
+	pDirLight->AddComponent<leap::DirectionalLightComponent>()->GetTransform()->SetWorldRotation(0, 45, 45, true);
 }
